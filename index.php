3fc8ce67403c5472f6a4d5d6a67e005eacc110ba
cynthia <cv334852@gmail.com>
2017-03-04 21:36:10 +0000

    i love melanie martinez

--- index.php
+++ index.php
@@ -4,10 +4,432 @@
 <!-- set default language -->
 <html lang="en">
 <!-- header: stuff we reference will go here (Libraries, Stylesheet stuff) -->
-<head>  
+<head> 
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1"> 
+
+  <!-- These scripts give us access to the bootstrap font end framework, which makes styling pretty easy!  -->
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 </head> 
 <!-- body: where we will put the stuff we can see! -->
 <body>
-<h1>Hello World!</h1>     
-</body>
+  <!-- suddenly, we can style our stuff with bootstrap classes!  -->
+    <h1>Hello World!</h1> 
+      
+</body><!-- This is the basic webpage-->
+<!-- We call it a hypertext markup language document -->
+<!DOCTYPE html>
+<!-- set default language -->
+<html lang="en">
+<!-- header: stuff we reference will go here (Libraries, Stylesheet stuff) -->
+<head> 
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1"> 
 
+  <!-- These scripts give us access to the bootstrap font end framework, which makes styling pretty easy!  -->
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+</head> 
+<!-- body: where we will put the stuff we can see! -->
+<body>
+  <!-- suddenly, we can style our stuff with bootstrap classes!  -->
+    <h1>Hello World!</h1> 
+      
+</body><!-- This is the basic webpage-->
+<!-- We call it a hypertext markup language document -->
+<!DOCTYPE html>
+<!-- set default language -->
+<html lang="en">
+<!-- header: stuff we reference will go here (Libraries, Stylesheet stuff) -->
+<head> 
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1"> 
+
+  <!-- These scripts give us access to the bootstrap font end framework, which makes styling pretty easy!  -->
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+</head> 
+<!-- body: where we will put the stuff we can see! -->
+<body>
+  <!-- suddenly, we can style our stuff with bootstrap classes!  -->
+    <h1>Hello World!</h1> 
+      
+</body><!-- This is the basic webpage-->
+<!-- We call it a hypertext markup language document -->
+<!DOCTYPE html>
+<!-- set default language -->
+<html lang="en">
+<!-- header: stuff we reference will go here (Libraries, Stylesheet stuff) -->
+<head> 
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1"> 
+
+  <!-- These scripts give us access to the bootstrap font end framework, which makes styling pretty easy!  -->
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+</head> 
+<!-- body: where we will put the stuff we can see! -->
+<body>
+  <!-- suddenly, we can style our stuff with bootstrap classes!  -->
+    <h1>Hello World!</h1> 
+      
+</body><!-- This is the basic webpage-->
+<!-- We call it a hypertext markup language document -->
+<!DOCTYPE html>
+<!-- set default language -->
+<html lang="en">
+<!-- header: stuff we reference will go here (Libraries, Stylesheet stuff) -->
+<head> 
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1"> 
+
+  <!-- These scripts give us access to the bootstrap font end framework, which makes styling pretty easy!  -->
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+</head> 
+<!-- body: where we will put the stuff we can see! -->
+<body>
+  <!-- suddenly, we can style our stuff with bootstrap classes!  -->
+    <h1>Hello World!</h1> 
+      
+</body>!-- This is the basic webpage-->
+<!-- We call it a hypertext markup language document -->
+<!DOCTYPE html>
+<!-- set default language -->
+<html lang="en">
+<!-- header: stuff we reference will go here (Libraries, Stylesheet stuff) -->
+<head> 
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1"> 
+
+  <!-- These scripts give us access to the bootstrap font end framework, which makes styling pretty easy!  -->
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+</head> 
+<!-- body: where we will put the stuff we can see! -->
+<body>
+  <!-- let's add more! We'll put in some sections that we can fill with our examples!  -->
+    <h1>My Page</h1>
+    <div id = "section">
+  
+    <div class= "container">
+      <div class = "jumbotron">
+        <h2>First Chart Type</h2>
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        
+      </div>
+    </div>  
+    
+    <div class= "container">
+      <div class = "jumbotron">
+        <h2>Second Chart Type</h2>
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        
+      </div>  
+    </div>
+
+    <div class= "container">
+      <div class = "jumbotron">
+        <h2>Third Chart Type</h2>
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        
+      </div>  
+    </div> 
+
+    <div class= "container">
+      <div class = "jumbotron">
+        <h2>Fourth Chart Type (Not a chart!)</h2>
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        <h2>Video</h2>
+        
+        
+      </div>  
+    </div> 
+
+  </div>
+<!-- End Charts -->
+      
+</body><!-- This is the basic webpage--> 
+<!-- We call it a hypertext markup language document -->
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1"> 
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+</head>
+<body>
+  <!-- Let's add header and footer!  -->
+  <div id= "navbar"> 
+     <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <h1> My Page </h1>
+        </div>
+      </nav>
+   </div>   
+  <div id = "section">
+  
+    <div class= "container">
+      <div class = "jumbotron">
+        <h2>First Chart Type</h2>
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        
+      </div>
+    </div>  
+    
+    <div class= "container">
+      <div class = "jumbotron">
+        <h2>Second Chart Type</h2>
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        
+      </div>  
+    </div>
+
+    <div class= "container">
+      <div class = "jumbotron">
+        <h2>Third Chart Type</h2>
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        
+      </div>  
+    </div> 
+
+    <div class= "container">
+      <div class = "jumbotron">
+        <h2>Fourth Chart Type (Not a chart!)</h2>
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        <h2>Video</h2>
+        
+        
+      </div>  
+    </div> 
+
+  </div>
+<!-- Let's add header & Footer -->
+  <footer>
+      <div class="container">
+        <div id="section">
+          <div class="row">
+            <p class="copyright"> &copy 2017 Friendly Fulcrum </p>
+          </div>
+        </div>  
+      </div>
+  </footer>
+      
+</body><!-- This is the basic webpage--> 
+<!-- We call it a hypertext markup language document -->
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1"> 
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+</head>
+<body>
+  <!-- Let's add header and footer!  -->
+  <div id= "navbar"> 
+     <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <h1> My Page </h1>
+        </div>
+      </nav>
+   </div>   
+  <div id = "section">
+  
+    <div class= "container">
+      <div class = "jumbotron">
+        <h2>First Chart Type</h2>
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        
+      </div>
+    </div>  
+    
+    <div class= "container">
+      <div class = "jumbotron">
+        <h2>Second Chart Type</h2>
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        
+      </div>  
+    </div>
+
+    <div class= "container">
+      <div class = "jumbotron">
+        <h2>Third Chart Type</h2>
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        
+      </div>  
+    </div> 
+
+    <div class= "container">
+      <div class = "jumbotron">
+        <h2>Fourth Chart Type (Not a chart!)</h2>
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        <h2>Video</h2>
+        
+        
+      </div>  
+    </div> 
+
+  </div>
+<!-- Let's add header & Footer -->
+  <footer>
+      <div class="container">
+        <div id="section">
+          <div class="row">
+            <p class="copyright"> &copy 2017 Friendly Fulcrum </p>
+          </div>
+        </div>  
+      </div>
+  </footer>
+      
+</body><!-- This is the basic webpage--> 
+<!-- We call it a hypertext markup language document -->
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1"> 
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+</head>
+<body>
+  <!-- Let's add header and footer!  -->
+  <div id= "navbar"> 
+     <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <h1> My Page </h1>
+        </div>
+      </nav>
+   </div>   
+  <div id = "section">
+  
+    <div class= "container">
+      <div class = "jumbotron">
+        <h2>First Chart Type</h2>
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        
+      </div>
+    </div>  
+    
+    <div class= "container">
+      <div class = "jumbotron">
+        <h2>Second Chart Type</h2>
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        
+      </div>  
+    </div>
+
+    <div class= "container">
+      <div class = "jumbotron">
+        <h2>Third Chart Type</h2>
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        
+      </div>  
+    </div> 
+
+    <div class= "container">
+      <div class = "jumbotron">
+        <h2>Fourth Chart Type (Not a chart!)</h2>
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        <h2>Video</h2>
+        
+        
+      </div>  
+    </div> 
+
+  </div>
+<!-- Let's add header & Footer -->
+  <footer>
+      <div class="container">
+        <div id="section">
+          <div class="row">
+            <p class="copyright"> &copy 2017 Friendly Fulcrum </p>
+          </div>
+        </div>  
+      </div>
+  </footer>
+      
+</body><!-- This is the basic webpage--> 
+<!-- We call it a hypertext markup language document -->
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1"> 
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+</head>
+<body>
+  <!-- Let's add header and footer!  -->
+  <div id= "navbar"> 
+     <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <h1> My Page </h1>
+        </div>
+      </nav>
+   </div>   
+  <div id = "section">
+  
+    <div class= "container">
+      <div class = "jumbotron">
+        <h2>First Chart Type</h2>
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        
+      </div>
+    </div>  
+    
+    <div class= "container">
+      <div class = "jumbotron">
+        <h2>Second Chart Type</h2>
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        
+      </div>  
+    </div>
+
+    <div class= "container">
+      <div class = "jumbotron">
+        <h2>Third Chart Type</h2>
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        
+      </div>  
+    </div> 
+
+    <div class= "container">
+      <div class = "jumbotron">
+        <h2>Fourth Chart Type (Not a chart!)</h2>
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        <h2>Video</h2>
+        
+        
+      </div>  
+    </div> 
+
+  </div>
+<!-- Let's add header & Footer -->
+  <footer>
+      <div class="container">
+        <div id="section">
+          <div class="row">
+            <p class="copyright"> &copy 2017 Friendly Fulcrum </p>
+          </div>
+        </div>  
+      </div>
+  </footer>
+      
+</body>
